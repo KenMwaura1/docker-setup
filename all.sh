94979dbaee7050147fd500a3134cb87c301746c9
KenMwaura1 <kemwaura@gmail.com>
2023-01-27 22:03:26 +0300

    updated workflow

--- all.sh
+++ all.sh
@@ -19,3 +19,21 @@ find $DIR -type f -name "*.sh" -exec chmod +x {} \;
 echo "Running all shell scripts in the current directory:"
 find $DIR -type f -name "*.sh" -exec basename {} \; -exec sh {} \;
 
+# kill running processes
+echo "Killing running processes:"
+ps -ef | grep -v grep | grep -i "docker" | awk '{print $2}' | xargs kill -9
+
+# If container exists then skip creation
+docker ps -a | grep -q "postgres" && echo "Container exists" || echo "Container does not exist"
+
+# If container exists then skip creation
+docker ps -a | grep -q "pgadmin4" && echo "Container exists" || echo "Container does not exist"
+
+# If container exists then skip creation
+docker ps -a | grep -q "redis" && echo "Container exists" || echo "Container does not exist"
+
+# If container exists then skip creation
+docker ps -a | grep -q "mongo-db" && echo "Container exists" || echo "Container does not exist"
+
+# If container exists then skip creation
+docker ps -a | grep -q "mysql" && echo "Container exists" || echo "Container does not exist"
