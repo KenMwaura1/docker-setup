b5aeb2acd64ea65c6e3845b87819df4f86a03458
KenMwaura1 <kemwaura@gmail.com>
2023-05-07 16:45:46 +0300

    fixed name issue

--- grafana.sh
+++ grafana.sh
@@ -1,2 +1,2 @@
 # command for starting up local grafana container
-docker run -d -p 3000:3000 grafana/grafana-oss --name grafana
+docker run -d -p 3000:3000 --name grafana grafana/grafana-oss
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,1 +1,2 @@
 docker run -d --hostname zoo-rabbit --name z-rabbit -e RABBITMQ_DEFAULT_USER=zoo -e RABBITMQ_DEFAULT_PASS=z-pass rabbitmq:3-management
+
